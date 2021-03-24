50bd84cbbefa1f5253084f6fc9085ccc8711de27
Indra41192478 <71803839+Indra41192478@users.noreply.github.com>
2021-03-25 00:44:33 +0700

    tugas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ManagementUserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//untuk index menu utama
+//Route::get('/', [ManagementUserController::class, 'index']);
+
+//untuk index di sub menu
+Route::resource('user', ManagementUserController::class);
